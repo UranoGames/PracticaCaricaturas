e1578ae92c45eda0bd82e4a68788d3e66fcc9b66
UranoGames <110320784+UranoGames@users.noreply.github.com>
2023-04-27 08:24:37 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,6 @@ self.addEventListener('install', e => {
     const cacheInstallEstatico = caches.open(cache_estatico).then(cache => {
 
         return cache.addAll([
-        '/',
         '/index.html'
                    
         ]);
